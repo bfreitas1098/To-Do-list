bf38e80c8230e4d7e2e948f7f3a08bfb0773fa0c
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-08 21:39:04 -0500

    Added basic content and styling to the app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,47 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header>My To-Do List</header>
+      <header>To-Do List</header>
+      <blockquote>
+        <p>"Action is the foundational key to all success." - Pablo Picasso</p>
+      </blockquote>
+      <form>
+        <input type="text" autoComplete="off" autoFocus="on" />
+        <input type="submit" value="Add" />
+      </form>
+      <br />
+      <span>
+        <button>Show All Tasks</button>
+        <button>Show Active Tasks</button>
+        <button>Show Completed Tasks</button>
+      </span>
+      <h3>4 Tasks Remaining</h3>
+      <ul>
+        <li>
+          <span>
+            <input type="checkbox" />
+            Take out trash
+          </span>
+        </li>
+        <li>
+          <span>
+            <input type="checkbox" />
+            Walk dog
+          </span>
+        </li>
+        <li>
+          <span>
+            <input type="checkbox" />
+            Put away laundry
+          </span>
+        </li>
+        <li>
+          <span>
+            <input type="checkbox" />
+            Finish homework
+          </span>
+        </li>
+      </ul>
     </div>
   );
 }
